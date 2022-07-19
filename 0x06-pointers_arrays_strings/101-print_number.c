713adcc61d20a3915495a6aeb41549470d84b525
Progress <leswifil@gmail.com>
2022-07-19 01:46:21 -0700

    fixing

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,31 +1,28 @@
 #include "main.h"
 
-void print_uint(unsigned int n);
-
 /**
-* print_number - Prints an integer
-* @n: The integer to print
-*/
+ * print_number - prints an integer using _putchar
+ * @n: integer to be printed
+ */
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
+	unsigned int num;
 
-print_uint((unsigned int) n);
-}
-
-/**
-* print_uint - Prints an unsigned integer
-* @n: The unsigned int to print
-*/
-void print_uint(unsigned int n)
-{
-if (n / 10 != 0)
-print_uint(n / 10);
-_putchar(n % 10 + '0');
+	/* print - infront of negative numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	/* print the digits infront of the current digit */
+	if (num / 10)
+		print_number(num / 10);
+	/* print current digit */
+	_putchar((num % 10) + '0');
 }
 
 
